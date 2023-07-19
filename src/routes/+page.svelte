d04493f07c3001631c50267ea729bec4248b598f
absorian <07221432bs@gmail.com>
2023-07-19 23:00:19 +0300

    add demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,21 @@
 	import { Canvas } from "$lib";
 	import type { Pixel } from "$lib";
 
+	// demo
+	const arr_size: number = 1000;
+	let arr: Pixel[][] = []
+
+	arr.length = arr_size;
+	for(let i = 0; i < arr_size; i++) {
+		arr[i] = []
+		arr[i].length = arr_size;
+		for(let j = 0; j < arr_size; j++) {
+			arr[i][j] = {
+				color: (i + j % 2) % 2 == 0 ? "gray" : "black"
+			}
+		}
+	}
 </script>
 
-<Canvas/>
+<Canvas pixels={arr} selected_color={"#aa55f1"}/>
 
